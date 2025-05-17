25ca71236d908f11f37792033cbc6b2830ec36c2
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2025-05-17 09:37:47 +0100

    clerk user created webhook implemented

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,58 +1,33 @@
 // app/api/webhooks/clerk/route.ts
-import { Webhook } from "svix";
-import { headers } from "next/headers";
-import { WebhookEvent } from "@clerk/nextjs/server";
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { createUser } from "@/server/user/create-user.action";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function POST(req: Request) {
-  // Get the headers
-  const headerPayload = await headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
-
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
-  }
-
-  // Get the body
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
-
-  // Create a new Svix instance with your webhook secret
-  const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET || "");
-
-  let evt: WebhookEvent;
-
-  // Verify the payload
+export async function POST(req: NextRequest) {
   try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) as WebhookEvent;
+    const evt = await verifyWebhook(req);
+    console.log("🔔 Webhook received!", evt.type);
+
+    // Handle the webhook
+    if (evt.type === "user.created") {
+      const { id, username } = evt.data;
+      const result = await createUser(id, username);
+
+      if (!result.success) {
+        return NextResponse.json(
+          { error: "Error creating user" },
+          { status: 500 }
+        );
+      }
+    }
+
+    return NextResponse.json({ success: true });
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
+    return NextResponse.json(
+      { error: "Error verifying webhook" },
+      { status: 400 }
+    );
   }
-
-  // Handle the webhook
-  const eventType = evt.type;
-  if (eventType === "user.created") {
-    const { id, username, ...attributes } = evt.data;
-
-    const result = await createUser(id, username);
-    if (!result.success) {
-      return new Response("Error creating user", {
-        status: 500,
-      });
-    }
-  }
-
-  return new Response("", { status: 200 });
 }
